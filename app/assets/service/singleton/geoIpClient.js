ead17ede7dc652c854336e738f8bd08d26b2fdbf
Daniela Guerra <daniela.guerra@mpt.mp.br>
2019-09-20 08:17:55 -0300

    Removing unused functions from geoipclient.

--- app/assets/service/singleton/geoIpClient.js
+++ app/assets/service/singleton/geoIpClient.js
@@ -48,43 +48,6 @@ class GeoIpClient  {
             }
         }
 
-        errorLocationInfo (err){
-            console.log(err);
-            let errorObj = {status: "fail", message: "Não foi possível identificar localização."};
-            this.callback(errorObj);
-        }
-
-        displayLocationInfo(position) {
-            const lng = position.coords.longitude;
-            const lat = position.coords.latitude;
-    
-            // console.log(`longitude: ${ lng } | latitude: ${ lat }`);
-
-            var ajaxCall = {
-                method: "GET",
-                //"url": "http://ip-api.com/json/?fields=country,region,city,lat,lon,isp,query,status,message",
-                "url": "https://nominatim.openstreetmap.org/reverse?lat="+lat+"&lon="+lng+"&format=json&accept-language=pt-br",
-                headers: {}
-            };
-            let callback = this.callback;
-            axios(ajaxCall)
-            .then(
-                result => {
-                    //console.log(result.data);
-                    callback(result.data.address)
-                },
-                error => {
-                    var errorObj = {status: "fail", message: "Não foi possível identificar localização."};
-                    callback(errorObj);
-                }
-            )
-            .catch(function (error) {
-                // handle error
-                var errorObj = {status: "fail", message: "Não foi possível identificar localização."};
-                callback(errorObj);
-            });
-
-        }
   }
 
 export default GeoIpClient;
