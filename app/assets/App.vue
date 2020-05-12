e6e46f2365c0eb993782d28e8a44f3911af4ac67
Daniela de Miranda Guerra <daniela.guerra@mpt.mp.br>
2020-05-12 16:11:56 -0300

    Fixing obs covid link in App.vue

--- app/assets/App.vue
+++ app/assets/App.vue
@@ -569,7 +569,7 @@
             blocked: false,
             rippleColor: 'deep-purple--text darken-2' },
           { app_icon: 'covid', title: 'COVID-19',
-            to: '/codiv', external: false,
+            to: '/covid', external: false,
             blocked: false,
             rippleColor: 'deep-orange--text darken-2' },
           // { icon: 'flight_takeoff', title: 'Migrações e Trabalho',
