cb11b91b5679ef67420a286d44acd42d7ed8eada
Daniela de Miranda Guerra <daniela.guerra@mpt.mp.br>
2021-10-15 12:27:18 -0300

    Adjusting generateSiteMap script

--- scripts/generateSiteMap.py
+++ scripts/generateSiteMap.py
@@ -51,15 +51,39 @@ def generate_structure(view_conf_path):
         obs_item_yaml = read_yaml(view_conf_path + "\\observatorio\\" + obs_item['id'] + ".yaml")
         obs_search_text = 'Apresentação - ' + obs_item['name']
         for item_desc_obs in obs_item_yaml['prevalencia']['description']:
-            if(item_desc_obs['type']== 'text'):
+            if (item_desc_obs['type'] == 'text'):
                 if (('title' in item_desc_obs) and (item_desc_obs['title'] != '')):
                     obs_search_text += ' ' + item_desc_obs['title']
                 if ('fixed' in item_desc_obs['content']):
                     obs_search_text += ' ' + item_desc_obs['content']['fixed']
                 if ('template' in item_desc_obs['content']):
                     obs_search_text += ' ' + item_desc_obs['content']['template']
+            if (item_desc_obs['type'] ==  'switch-group'):
+                for switch in item_desc_obs['switches']:
+                    if ('label' in switch):
+                        obs_search_text += ' ' + switch['label']
+                    if ('minicards' in switch):
+                        for minicard in switch['minicards']:
+                            for arg in minicard['args']:
+                                if (arg['prop'] != 'value'):
+                                    if ('fixed' in arg):
+                                        obs_search_text += ' ' + arg['fixed']
+                                    if ('template' in arg):
+                                        obs_search_text += ' ' + arg['template']
+            if (item_desc_obs['type'] ==  'radio'):
+                for item in item_desc_obs['items']:
+                    if ('label' in item):
+                        obs_search_text += ' ' + item['label']
+                    if ('minicards' in item):
+                        for minicard in item['minicards']:
+                            for arg in minicard['args']:
+                                if (arg['prop'] != 'value'):
+                                    if ('fixed' in arg):
+                                        obs_search_text += ' ' + arg['fixed']
+                                    if ('template' in arg):
+                                        obs_search_text += ' ' + arg['template']
         for item_desc_obs in obs_item_yaml['prevalencia']['description_right']:
-            if(item_desc_obs['type']== 'text'):
+            if (item_desc_obs['type']== 'text'):
                 if (('title' in item_desc_obs) and (item_desc_obs['title'] != '')):
                     obs_search_text += ' ' + item_desc_obs['title']
                 if ('fixed' in item_desc_obs['content']):
@@ -67,8 +91,43 @@ def generate_structure(view_conf_path):
                 if ('template' in item_desc_obs['content']):
                     obs_search_text += ' ' + item_desc_obs['content']['template']
     
-        dimensions.append({'id': obs_item['id'] + '_apresentacao', 'type':'obs_apresentacao','name': 'Apresentação - Observatório ' + obs_item['short_title'], 'url': obs_item['url'], 'search_text': obs_search_text})
-        dimensions.append({'id': obs_item['id'] + '_smartmap', 'type':'obs_smartmap','name': 'Smartmap - Mapa avançado - Observatório ' + obs_item['short_title'], 'url': obs_item['url'] + '/smartmap', 'search_text': 'Smartmap - Mapa avançado - ' + obs_item['name']})
+        dimensions.append({'id': obs_item['id'] + '_apresentacao', 'type':'obs_apresentacao','name': 'Apresentação - Observatório ' + obs_item['short_title'], 'url': obs_item['url'], 'search_text': obs_search_text.replace('<br/>', ' ')})
+
+        # Smartmap search_text
+        obs_search_text = 'Smartmap - Mapa avançado - ' + obs_item['name']
+        for item_desc_obs in obs_item_yaml['prevalencia']['mapa_filtros']:
+            if (item_desc_obs['type'] == 'text'):
+                if (('title' in item_desc_obs) and (item_desc_obs['title'] != '')):
+                    obs_search_text += ' ' + item_desc_obs['title']
+                if ('fixed' in item_desc_obs['content']):
+                    obs_search_text += ' ' + item_desc_obs['content']['fixed']
+                if ('template' in item_desc_obs['content']):
+                    obs_search_text += ' ' + item_desc_obs['content']['template']
+            if (item_desc_obs['type'] ==  'switch-group'):
+                for switch in item_desc_obs['switches']:
+                    if ('label' in switch):
+                        obs_search_text += ' ' + switch['label']
+                    if ('minicards' in switch):
+                        for minicard in switch['minicards']:
+                            for arg in minicard['args']:
+                                if (arg['prop'] != 'value'):
+                                    if ('fixed' in arg):
+                                        obs_search_text += ' ' + arg['fixed']
+                                    if ('template' in arg):
+                                        obs_search_text += ' ' + arg['template']
+            if (item_desc_obs['type'] ==  'radio'):
+                for item in item_desc_obs['items']:
+                    if ('label' in item):
+                        obs_search_text += ' ' + item['label']
+                    if ('minicards' in item):
+                        for minicard in item['minicards']:
+                            for arg in minicard['args']:
+                                if (arg['prop'] != 'value'):
+                                    if ('fixed' in arg):
+                                        obs_search_text += ' ' + arg['fixed']
+                                    if ('template' in arg):
+                                        obs_search_text += ' ' + arg['template']
+        dimensions.append({'id': obs_item['id'] + '_smartmap', 'type':'obs_smartmap','name': 'Smartmap - Mapa avançado - Observatório ' + obs_item['short_title'], 'url': obs_item['url'] + '/smartmap', 'search_text': obs_search_text.replace('<br/>', ' ')})
 
         # Dimensões
         for dim_item in dimensions_yaml['dimensoes']:
@@ -95,7 +154,7 @@ def generate_structure(view_conf_path):
                                     card_search_text += ' ' + item_desc['content']['fixed']
                                 if ('template' in item_desc['content']):
                                     card_search_text += ' ' + item_desc['content']['template']
-                    cards.append({'id': card['id'], 'type':'card', 'name': card['title']['fixed'], 'url': dim_item['to'] + '#' + card['id'],'search_text': card_search_text})
+                    cards.append({'id': card['id'], 'type':'card', 'name': card['title']['fixed'], 'url': dim_item['to'] + '#' + card['id'],'search_text': card_search_text.replace('<br/>', ' ')})
             dimensions.append({'id': dim_item['id'], 'type':'dimensao','name':dim_item['short_desc'], 'url': dim_item['to'], 'search_text': dim_item['label'], 'children': cards})
         obs_dim.append({**obs_item, 'children': dimensions.copy()})
 
@@ -114,7 +173,7 @@ def generate_structure(view_conf_path):
                             about_text += ' ' + ' '.join(item_list['values'])
             if(item_section['type'] == 'text'):
                 about_text += ' ' + item_section['content']
-    obs_dim.append({'id': 'smartlab_sobre', 'type':'sobre','name': 'Sobre a Iniciativa Smartlab (Iniciativa, Equipe e Créditos)', 'url': '/saibamais/smartlab', 'search_text' : about_text})
+    obs_dim.append({'id': 'smartlab_sobre', 'type':'sobre','name': 'Sobre a Iniciativa Smartlab (Iniciativa, Equipe e Créditos)', 'url': '/saibamais/smartlab', 'search_text' : about_text.replace('<br/>', ' ')})
     return obs_dim
 
 try:
