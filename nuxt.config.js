c76dae59180422fdb95ecd39e8ce02e283c732fd
Daniela de Miranda Guerra <daniela.guerra@mpt.mp.br>
2022-12-15 15:26:50 -0300

    chore: Adjusting leaflet

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,7 +29,8 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    "~/assets/app.styl"    
+    '~/assets/app.styl',
+    'leaflet/dist/leaflet.css'    
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
