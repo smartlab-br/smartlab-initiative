bfafa917faaac5f902c9fbdefd23e605ca96cc94
Daniela de Miranda Guerra <daniela.guerra@mpt.mp.br>
2024-09-10 12:29:32 -0300

    Adjusting plugins

--- types/plugins.d.ts
+++ types/plugins.d.ts
@@ -1,7 +1,7 @@
-declare module '#app' {
+declare module "#app" {
   interface NuxtApp {
     // viewConfReader functions
-    async $loadYamlArray(currentStruct: any, yamlArray: any[], finalCbFunction: (context: any) => void): void
+    $loadYamlArray(currentStruct: any, yamlArray: any[], finalCbFunction: (context: any) => void): Promise<void>
     $fillDataStructure(structure: any, customParams: any, cbFunction: (dataset: Record<string, any> | string, rules: Record<string, any>, structure: Record<string, any>, addedParams?: Record<string, any>, metadata?: Record<string, any>) => void, addedParams?: any): void
     $sendDataStructureError(error: string = "Falha ao carregar dados do componente.")
     $reformDataset(dataset: any, reformOptions: any, customParams: any = {}): Record<string, any>
@@ -17,25 +17,25 @@ declare module '#app' {
     $chartRegen(store: MainStore, chartHandler: any, id: string, chartType: string, structure: any, chartOptions: any, dataset: any, metadata: any, sectionIndex: number = 0): Promise<unknown> | undefined
     $buildChartAdditionalOptions(store: MainStore, chartType: string, structure: any, chartOptions: any, dataset: any, metadata: any, sectionIndex: number = 0): any
     $changeCursor(containerId: string, image: string): void
-    async $obsTETooltip(target: any, route: RouteLocationNormalizedLoaded, store: MainStore, _tooltip_list: string[] = [], _removed_text_list: string[] = [], options: any = null): Promise<void>
-    async $obsTITooltip(target: any, route: RouteLocationNormalizedLoaded, store: MainStore, _tooltip_list: string[] = [], _removed_text_list: string[] = [], options: any = null): Promise<void>
-    async $obsSSTTooltip(target: any, route: RouteLocationNormalizedLoaded, store: MainStore, _tooltip_list: string[] = [], _removed_text_list: string[] = [], options: any = null): Promise<void>
-    async $obsTDTooltip(target: any, route: RouteLocationNormalizedLoaded, store: MainStore, _tooltip_list: string[] = [], _removed_text_list: string[] = [], options: any = null): Promise<void>
+    $obsTETooltip(target: any, route: RouteLocationNormalizedLoaded, store: MainStore, _tooltip_list: string[] = [], _removed_text_list: string[] = [], options: any = null): Promise<void>
+    $obsTITooltip(target: any, route: RouteLocationNormalizedLoaded, store: MainStore, _tooltip_list: string[] = [], _removed_text_list: string[] = [], options: any = null): Promise<void>
+    $obsSSTTooltip(target: any, route: RouteLocationNormalizedLoaded, store: MainStore, _tooltip_list: string[] = [], _removed_text_list: string[] = [], options: any = null): Promise<void>
+    $obsTDTooltip(target: any, route: RouteLocationNormalizedLoaded, store: MainStore, _tooltip_list: string[] = [], _removed_text_list: string[] = [], options: any = null): Promise<void>
     $tooltipLinkGoogleStreetView(target: any, route: RouteLocationNormalizedLoaded, store: MainStore, tooltip_list: string[] = [], removed_text_list: string[] = [], options: any = null): void
-    async $obsCovidMunicipioTooltip(target: any, route: RouteLocationNormalizedLoaded, store: MainStore, _tooltip_list: string[] = [], _removed_text_list: string[] = [], options: any = null): Promise<void>
+    $obsCovidMunicipioTooltip(target: any, route: RouteLocationNormalizedLoaded, store: MainStore, _tooltip_list: string[] = [], _removed_text_list: string[] = [], options: any = null): Promise<void>
     $obsCustomTooltip(target: any, route: RouteLocationNormalizedLoaded, tooltip_list: string[] = [], removed_text_list: string[] = [], options: any = null): void
     $showDefaultTooltip(sourceDS: any, _rules: any, _sourceStructure: any, addedParams: any = null, _metadata: any = null): void
     $obsCovidRegicTooltip(target: any, _route: RouteLocationNormalizedLoaded, _tooltip_list: string[] = [], _removed_text_list: string[] = [], _options: any = null): void
     $obsCovidRegicUTITooltip(target: any, _route: RouteLocationNormalizedLoaded, _tooltip_list: string[] = [], _removed_text_list: string[] = [], _options: any = null): void
-    async $obsCovidRegicTextTooltip(target: any, showDadosSaude: boolean = false): Promise<void>
+    $obsCovidRegicTextTooltip(target: any, showDadosSaude: boolean = false): Promise<void>
     $getEscopo(idLocalidade: string): "brasil" | "mptreg" | "prtptm" | "regiao" | "estado" | "mesorregiao" | "microrregiao" | "municipio"
   }
 }
 
-declare module 'vue' {
+declare module "vue" {
   interface ComponentCustomProperties {
     // viewConfReader functions
-    async $loadYamlArray(currentStruct: any, yamlArray: any[], finalCbFunction: (context: any) => void): void
+    $loadYamlArray(currentStruct: any, yamlArray: any[], finalCbFunction: (context: any) => void): Promise<void>
     $fillDataStructure(structure: any, customParams: any, cbFunction: (dataset: Record<string, any> | string, rules: Record<string, any>, structure: Record<string, any>, addedParams?: Record<string, any>, metadata?: Record<string, any>) => void, addedParams?: any): void
     $sendDataStructureError(error: string = "Falha ao carregar dados do componente.")
     $reformDataset(dataset: any, reformOptions: any, customParams: any = {}): Record<string, any>
@@ -51,17 +51,17 @@ declare module 'vue' {
     $chartRegen(store: MainStore, chartHandler: any, id: string, chartType: string, structure: any, chartOptions: any, dataset: any, metadata: any, sectionIndex: number = 0): Promise<unknown> | undefined
     $buildChartAdditionalOptions(store: MainStore, chartType: string, structure: any, chartOptions: any, dataset: any, metadata: any, sectionIndex: number = 0): any
     $changeCursor(containerId: string, image: string): void
-    async $obsTETooltip(target: any, route: RouteLocationNormalizedLoaded, store: MainStore, _tooltip_list: string[] = [], _removed_text_list: string[] = [], options: any = null): Promise<void>
-    async $obsTITooltip(target: any, route: RouteLocationNormalizedLoaded, store: MainStore, _tooltip_list: string[] = [], _removed_text_list: string[] = [], options: any = null): Promise<void>
-    async $obsSSTTooltip(target: any, route: RouteLocationNormalizedLoaded, store: MainStore, _tooltip_list: string[] = [], _removed_text_list: string[] = [], options: any = null): Promise<void>
-    async $obsTDTooltip(target: any, route: RouteLocationNormalizedLoaded, store: MainStore, _tooltip_list: string[] = [], _removed_text_list: string[] = [], options: any = null): Promise<void>
+    $obsTETooltip(target: any, route: RouteLocationNormalizedLoaded, store: MainStore, _tooltip_list: string[] = [], _removed_text_list: string[] = [], options: any = null): Promise<void>
+    $obsTITooltip(target: any, route: RouteLocationNormalizedLoaded, store: MainStore, _tooltip_list: string[] = [], _removed_text_list: string[] = [], options: any = null): Promise<void>
+    $obsSSTTooltip(target: any, route: RouteLocationNormalizedLoaded, store: MainStore, _tooltip_list: string[] = [], _removed_text_list: string[] = [], options: any = null): Promise<void>
+    $obsTDTooltip(target: any, route: RouteLocationNormalizedLoaded, store: MainStore, _tooltip_list: string[] = [], _removed_text_list: string[] = [], options: any = null): Promise<void>
     $tooltipLinkGoogleStreetView(target: any, route: RouteLocationNormalizedLoaded, store: MainStore, tooltip_list: string[] = [], removed_text_list: string[] = [], options: any = null): void
-    async $obsCovidMunicipioTooltip(target: any, route: RouteLocationNormalizedLoaded, store: MainStore, _tooltip_list: string[] = [], _removed_text_list: string[] = [], options: any = null): Promise<void>
+    $obsCovidMunicipioTooltip(target: any, route: RouteLocationNormalizedLoaded, store: MainStore, _tooltip_list: string[] = [], _removed_text_list: string[] = [], options: any = null): Promise<void>
     $obsCustomTooltip(target: any, route: RouteLocationNormalizedLoaded, tooltip_list: string[] = [], removed_text_list: string[] = [], options: any = null): void
     $showDefaultTooltip(sourceDS: any, _rules: any, _sourceStructure: any, addedParams: any = null, _metadata: any = null): void
     $obsCovidRegicTooltip(target: any, _route: RouteLocationNormalizedLoaded, _tooltip_list: string[] = [], _removed_text_list: string[] = [], _options: any = null): void
     $obsCovidRegicUTITooltip(target: any, _route: RouteLocationNormalizedLoaded, _tooltip_list: string[] = [], _removed_text_list: string[] = [], _options: any = null): void
-    async $obsCovidRegicTextTooltip(target: any, showDadosSaude: boolean = false): Promise<void>
+    $obsCovidRegicTextTooltip(target: any, showDadosSaude: boolean = false): Promise<void>
     $getEscopo(idLocalidade: string): "brasil" | "mptreg" | "prtptm" | "regiao" | "estado" | "mesorregiao" | "microrregiao" | "municipio"
   }
 }
